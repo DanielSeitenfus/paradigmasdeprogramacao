e298729e5b8b8f585c371f85b4db74551360c904
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2019-04-28 02:09:06 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,6 +1,7 @@
 odd(N) :- N mod 2 =\= 0.
 
 /*Defina um predicado recursivo hasN(L,N) que seja verdadeiro se L for uma lista de N elementos.*/
+/*Estava começando a fazer a questão e somente testei o que isso iria retornar, e para minha surpresa, funcionou. Porém não entendi porque ele decrementa e testa automaticamente se o N>0 */
 hasN([],N). /*condição de parada*/
 hasN([H|T],N) :-
 	hasN(T,N).
@@ -12,5 +13,10 @@ inc99([H|T],L2) :-
 	H1 is H + 99, /*Define H1 pois o valor a esquerda não pode estar instaciado*/
 	inc99(T, [H1|T2]). /*passa o valor de H1 como Head, assim ele adiciona na lista*/
 
-
+/*Defina um predicado recursivo incN(L1,L2,N), de forma que L2 seja uma lista com todos os elementos de L1 acrescidos da constante N.*/
+/*Minha primeira lógica foi essa, porém não funcionou*/
+incN([],L2,N).
+incN([H|T], L2, N) :-
+	H1 is H + N,
+	incN(T, [H1|T2], N).
 
