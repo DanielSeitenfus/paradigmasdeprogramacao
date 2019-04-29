f1bc00e23ab4a0d1775b7700a1c6d5654bd768a3
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2019-04-28 23:56:34 -0300

    Update l3.pl (Exercício 7)
    Ainda, correção de variáveis não utilizadas que estavam gerando Warnings.

--- t3/t3.pl
+++ t3/t3.pl
@@ -7,19 +7,19 @@ hasN([_|T],N) :-
 
 /*Defina um predicado recursivo inc99(L1,L2), */
 /*de forma que L2 seja uma lista com todos os elementos de L1 acrescidos da constante 99.*/
-inc99([],L2). /*condição de parada, quando L1 passar por todos elementos, ele retorna a lista2*/
-inc99([H|T],L2) :-
+inc99([],_). /*condição de parada, quando L1 passar por todos elementos, ele retorna a lista2*/
+inc99([H|T],_) :-
 	H1 is H + 99, /*Define H1 pois o valor a esquerda não pode estar instaciado*/
-	inc99(T, [H1|T2]). /*passa o valor de H1 como Head, assim ele adiciona na lista*/
+	inc99(T, [H1|_]). /*passa o valor de H1 como Head, assim ele adiciona na lista*/
 
 /*Defina um predicado recursivo incN(L1,L2,N), de forma que L2 seja uma lista com todos os elementos de L1 acrescidos da constante N.*/
 /*Minha primeira lógica foi essa, porém não funcionou. Quando o L1 fica em [] ele não fez todas operações com o L2?*/
-incN2([],L2,N).
-incN2([H|T], L2, N) :-
+incN2([],_,_).
+incN2([H|T], _, N) :-
 	H1 is H + N,
-	incN2(T, [H1|T2], N).
+	incN2(T, [H1|_], N).
 
-incN([],[],N). /* Devo colocar L1 e L2 = [] como instrução de parada porque ele executa primeiro um depois o outro*/
+incN([],[],_). /* Devo colocar L1 e L2 = [] como instrução de parada porque ele executa primeiro um depois o outro*/
 incN([H|T], [H2|T2], N) :-
 	incN(T, T2, N),
 	H2 is H + N.
@@ -41,6 +41,13 @@ H2 = H1,
 par(H1),
 onlyEven(T1,T2); 
 onlyEven(T1,[H2|T2]). /* caso o par(H1) dê falso, perde-se a instância de H2(?) e executa-se onlyEven(T1, [H2|T2]) com H2 diferente de H1*/
+
+countdown(0,[]) :- !.
+countdown(N,[H|T]) :-
+	N1 is N - 1,
+	H = N,
+	countdown(N1, T).
+
 
 
 
