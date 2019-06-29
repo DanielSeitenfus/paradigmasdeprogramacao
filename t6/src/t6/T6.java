fa17ca68cde64a25719e773d53446f1ef006e3f4
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2019-06-29 01:23:32 -0300

    Update t6

--- t6/src/t6/T6.java
+++ t6/src/t6/T6.java
@@ -29,6 +29,7 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -36,6 +37,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -44,12 +46,11 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class T6 extends Application {
-
+    String url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTO06Jdr3J1kPYoTPRkdUaq8XuslvSD5--FPMht-ilVBT1gExJXDPTiX0P3FsrxV5VKUZJrIUtH1wvN/pub?gid=0&single=true&output=csv";
     List<String[]> lista;
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-
+    public void start(Stage primaryStage) {
         FlowPane flowPane = new FlowPane(Orientation.VERTICAL);
         flowPane.setAlignment(Pos.TOP_LEFT);
 
@@ -85,28 +86,18 @@ public class T6 extends Application {
             TableRow<Questao> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
                 String dados[] = lista.get(table.getSelectionModel().getFocusedIndex());
-                try {
-                    abreTelaInfos(primaryStage, new Questao(dados[1], dados[2], dados[3], dados[4], dados[5], dados[8], dados[9], dados[10], dados[11], dados[7], dados[13], dados[17]));
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(T6.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(T6.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                abreTelaInfos(primaryStage, new Questao(dados[1], dados[2], dados[3], dados[4], dados[5], dados[8], dados[9], dados[10], dados[11], dados[7], dados[13], dados[17]));
             });
             return row;
         });
 
         //MenusItem
         menuItem[0].setOnAction((ActionEvent event) -> { //Reload
-            try {
-                downloadTabela();
-                carregaTabela(table, true);
-            } catch (IOException ex) {
-                Logger.getLogger(T6.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            downloadTabela();
+            carregaTabela(table, true);
         });
         menuItem[1].setOnAction((ActionEvent event) -> { //Source
-            System.out.println("cheguei");
+            alteraUrl();
         });
         menuItem[2].setOnAction((ActionEvent event) -> { //Exit
             System.exit(0);
@@ -119,8 +110,25 @@ public class T6 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
+    private void alteraUrl(){
+        FlowPane flowPane = new FlowPane(Orientation.VERTICAL);
+        Label label = new Label("URL para download da planílha: ");
+        TextField tfUrl = new TextField(url);
+        Button botao = new Button("Alterar");
+        botao.setOnAction((event) -> {
+            url = tfUrl.getText();
+        });
+        flowPane.getChildren().addAll(label,tfUrl,botao);
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Redefinir URL de download");
+        newWindow.setScene(new Scene(flowPane));
+        newWindow.setHeight(140);
+        newWindow.setWidth(200);
+        newWindow.show();
+    }
 
-    private void abreTelaInfos(Stage primaryStage, Questao questao) throws FileNotFoundException, MalformedURLException, IOException {
+    private void abreTelaInfos(Stage primaryStage, Questao questao){
         FlowPane flowPaneGeral = new FlowPane(Orientation.HORIZONTAL);
         FlowPane flowPane = new FlowPane(Orientation.VERTICAL);
         flowPaneGeral.getChildren().add(flowPane);
@@ -150,11 +158,30 @@ public class T6 extends Application {
 
         if (!questao.getUrlcop().equals("")) {
             File file = new File("./src/imagens/" + questao.getIdquestao().getValue() + questao.getAno().getValue() + ".jpg");
-            if (!file.exists()) { //se o arquivo não existir, faz o download
-                InputStream in = new URL(questao.getUrlcop()).openStream();
-                Files.copy(in, Paths.get(file.getPath()), StandardCopyOption.REPLACE_EXISTING);
+            if (!file.exists()) {                 
+                InputStream in = null;
+                try {
+                    //se o arquivo não existir, faz o download
+                    in = new URL(questao.getUrlcop()).openStream();
+                    Files.copy(in, Paths.get(file.getPath()), StandardCopyOption.REPLACE_EXISTING);
+                } catch (MalformedURLException ex) {
+                    new Alert(Alert.AlertType.ERROR, "O download não pode ser realizado!").show();
+                } catch (IOException ex) {
+                    new Alert(Alert.AlertType.ERROR, "Algo não ocorreu bem!").show();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException ex) {
+                        Logger.getLogger(T6.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
-            Image image = new Image(new FileInputStream(file));
+            Image image = null;
+            try {
+                image = new Image(new FileInputStream(file));
+            } catch (FileNotFoundException ex) {
+                new Alert(Alert.AlertType.ERROR, "A imagem não pode ser carregada!").show();
+            }
             flowPaneGeral.getChildren().add(new ImageView(image));
         } else {
             flowPaneGeral.getChildren().add(new Label("Não há imagem."));
@@ -171,7 +198,7 @@ public class T6 extends Application {
         newWindow.show();
     }
 
-    private void carregaTabela(TableView table, boolean recarregar) throws FileNotFoundException, IOException {
+    private void carregaTabela(TableView table, boolean recarregar){
         if(recarregar){
             table.getItems().clear();
             table.getColumns().clear();
@@ -192,12 +219,17 @@ public class T6 extends Application {
         if (!file.exists()) {
             downloadTabela();
         }
-        this.lista = new CSVReaderHeaderAware(new FileReader(file)).readAll();
+        try {
+            this.lista = new CSVReaderHeaderAware(new FileReader(file)).readAll();
+        } catch (FileNotFoundException ex) {
+            new Alert(Alert.AlertType.ERROR, "O arquivo não pode ser carregado!").show();
+        } catch (IOException ex) {
+            new Alert(Alert.AlertType.ERROR, "Algo não ocorreu bem!").show();
+        }
         ObservableList<Questao> data = FXCollections.observableArrayList();
         for (String[] dados : lista) {
             data.add(new Questao(dados[1], dados[2], dados[3], dados[4], dados[5], dados[8], dados[9], dados[10], dados[11]));
         }
-        
         tableColumns[0].setCellValueFactory(new PropertyValueFactory("ano"));
         tableColumns[1].setCellValueFactory(new PropertyValueFactory("prova"));
         tableColumns[2].setCellValueFactory(new PropertyValueFactory("tipoquestao"));
@@ -211,18 +243,22 @@ public class T6 extends Application {
 
     }
 
-    private void downloadTabela() throws MalformedURLException, IOException {
+    private void downloadTabela() {
         Alert alerta = new Alert(Alert.AlertType.INFORMATION, "Baixando tabela...");
         alerta.show();
-        String url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTO06Jdr3J1kPYoTPRkdUaq8XuslvSD5--FPMht-ilVBT1gExJXDPTiX0P3FsrxV5VKUZJrIUtH1wvN/pub?gid=0&single=true&output=csv";
-        System.out.println("Download...");
-        InputStream in = new URL(url).openStream();
-        String pathArquivo = "./src/t6/enade.csv";
-        File file = new File(pathArquivo);
-        if (file.exists()) {
-            file.delete();
+        try {
+            InputStream in = new URL(url).openStream();
+            String pathArquivo = "./src/t6/enade.csv";
+            File file = new File(pathArquivo);
+            if (file.exists()) {
+                file.delete();
+            }
+            Files.copy(in, Paths.get(pathArquivo), StandardCopyOption.REPLACE_EXISTING);
+        } catch (MalformedURLException ex) {
+            new Alert(Alert.AlertType.ERROR, "Não foi possível encontrar URL!").show();
+        } catch (IOException ex) {
+            new Alert(Alert.AlertType.ERROR, "Algo não ocorreu bem!").show();
         }
-        Files.copy(in, Paths.get(pathArquivo), StandardCopyOption.REPLACE_EXISTING);
         alerta.close();
     }
 }
